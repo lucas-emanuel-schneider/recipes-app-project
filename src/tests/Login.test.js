9479766fd3e8125ea38c56370b5af7c1106d8d38
Lucas <lucas_emanuel_sc@hotmail.com>
2022-09-23 18:08:38 -0300

    organizando helpers mas nao terminado

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('Farewell, front-end', () => {
+test('testando componente Login', () => {
   // Este arquivo pode ser modificado ou deletado sem problemas
   render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputEmail = screen.getByPlaceholderText(/digite seu email/i);
+  const inputPassword = screen.getByPlaceholderText(/digite sua senha/i);
+  const buttonSubmit = screen.getByRole('button', {
+    name: /enviar/i,
+    value: false,
+  });
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputPassword).toBeInTheDocument();
+  expect(buttonSubmit).toBeInTheDocument();
 });
