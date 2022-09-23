dc4cb148481ed407c0f398ef62c19f8a94ca7494
Lucas <lucas_emanuel_sc@hotmail.com>
2022-09-23 16:54:52 -0300

    terminando as cofig basica e terminando a pag login  na main 'boa pratica'

---
+++ src/pages/Login.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+
+  const validateForm = () => {
+    const minPasswordLength = 6;
+    const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+    const isEmailValid = emailRegex.test(formData.email);
+    const isPasswordValid = formData.password.length >= minPasswordLength;
+    return !(isEmailValid && isPasswordValid);
+  };
+  const handleInputChanges = ({ target }) => {
+    const { name, value } = target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={ handleSubmit }>
+        <input
+          type="text"
+          name="email"
+          data-testid="email-input"
+          placeholder="Digite seu email"
+          onChange={ handleInputChanges }
+          value={ formData.email }
+        />
+        <input
+          type="password"
+          name="password"
+          data-testid="password-input"
+          onChange={ handleInputChanges }
+          placeholder="Digite sua senha"
+          value={ formData.password }
+        />
+        <button
+          type="submit"
+          disabled={ validateForm() }
+          data-testid="login-submit-btn"
+        >
+          Enviar
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
