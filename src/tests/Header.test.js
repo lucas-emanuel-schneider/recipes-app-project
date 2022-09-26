151c91497ac304c5809fac7f6b0ab8ff7b5aede3
tormelo <23294699+tormelo@users.noreply.github.com>
2022-09-26 16:34:34 -0300

    implementa testes para componente Header

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,60 +1,21 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './helpers/renderWithRouter';
 
-const testEmail = 'teste@trybe.com';
-const mealsRoute = '/meals';
-
 describe('Testa o componente Header', () => {
-  it('Testa se os componentes são renderizados corretamente', () => {
-    // Renderiza
-    const { history } = renderWithRouter(<App />);
-    // Verifica se a rota está correta
-    expect(history.location.pathname).toBe('/');
-    // Captura os elementos
-    const inputEmail = screen.getByPlaceholderText(/digite seu email/i);
-    const inputPassword = screen.getByPlaceholderText(/digite sua senha/i);
-    const buttonSubmit = screen.getByRole('button', {
-      name: /enviar/i,
-      value: false,
-    });
-    // Verifica se estão na tela
-    expect(inputEmail).toBeInTheDocument();
-    expect(inputPassword).toBeInTheDocument();
-    expect(buttonSubmit).toBeInTheDocument();
+  it('Deve redirecionar o usuário corretamente ao clicar no botão de perfil', () => {
+    const { history } = renderWithRouter(<App />, { initialEntries: ['/meals'] });
+
+    expect(history.location.pathname).toBe('/meals');
+    userEvent.click(screen.getByTestId('profile-top-btn'));
+    expect(history.location.pathname).toBe('/profile');
   });
-  it('Verifica se o botão é Habilitado se os valores estão corretos', () => {
-    // Renderiza
-    renderWithRouter(<App />);
-    // Captura os elementos
-    const inputEmail = screen.getByPlaceholderText(/digite seu email/i);
-    const inputPassword = screen.getByPlaceholderText(/digite sua senha/i);
-    const buttonSubmit = screen.getByRole('button', {
-      name: /enviar/i,
-    });
-    // escreve os valores no input
-    userEvent.type(inputEmail, testEmail);
-    userEvent.type(inputPassword, testEmail);
-    // Verifica se estão na tela
-    expect(buttonSubmit).toBeEnabled();
-  });
-  it('Verifica se ao logar corretamente a página, ela é redirecionada a /meals', () => {
-    // Renderiza
-    const { history } = renderWithRouter(<App />);
-    // Captura os elementos
-    const inputEmail = screen.getByPlaceholderText(/digite seu email/i);
-    const inputPassword = screen.getByPlaceholderText(/digite sua senha/i);
-    const buttonSubmit = screen.getByRole('button', {
-      name: /enviar/i,
-    });
-    // escreve os valores no input
-    userEvent.type(inputEmail, testEmail);
-    userEvent.type(inputPassword, testEmail);
-    // Clica no Botão de Submit
-    userEvent.click(buttonSubmit);
-    // Verifica se estão na tela
-    expect(history.location.pathname).toBe(mealsRoute);
+  it('Deve mostrar/esconder SearchBar corretamente', async () => {
+    renderWithRouter(<App />, { initialEntries: ['/meals'] });
+    expect(screen.queryByTestId('search-input')).toBeNull();
+    userEvent.click(screen.getByTestId('search-top-btn'));
+    expect(screen.getByTestId('search-input')).toBeInTheDocument();
   });
 });
