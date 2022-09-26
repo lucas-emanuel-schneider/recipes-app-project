b21f26914d2a1ddbe7eaa9a7c9fe166e3da3ee68
tormelo <23294699+tormelo@users.noreply.github.com>
2022-09-26 19:54:38 -0300

    refatora modo de req a api e inicia com requisicao sem filtros

---
+++ src/services/drinksAPI.js
@@ -0,0 +1,23 @@
+const drinksAPI = {
+  fetchByIngredient: async (ingredient) => {
+    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+    const { drinks } = await response.json();
+    return drinks;
+  },
+  fetchByName: async (name) => {
+    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
+    const { drinks } = await response.json();
+    return drinks;
+  },
+  fetchByFirstLetter: async (firstLetter) => {
+    if (firstLetter.length > 1) {
+      global.alert('Your search must have only 1 (one) character');
+      return;
+    }
+    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${firstLetter}`);
+    const { drinks } = await response.json();
+    return drinks;
+  },
+};
+
+export default drinksAPI;
