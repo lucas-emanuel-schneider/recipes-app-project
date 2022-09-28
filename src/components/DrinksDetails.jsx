ef9cbab4d482e066fd07a0c0936be0ec63c0b5a9
Lucas <lucas_emanuel_sc@hotmail.com>
2022-09-28 15:21:05 -0300

    começando a 29

--- src/components/DrinksDetails.jsx
+++ src/components/DrinksDetails.jsx
@@ -9,12 +9,13 @@ function DrinksDetails() {
     .filter((entrie) => entrie[1] !== null);
   const ingredients = details.filter((ing) => ing[0].includes('strIngredient'));
   const measures = details.filter((mea) => mea[0].includes('strMeasure'));
-  console.log(ingredients);
-  console.log(measures);
   return (
     recipeDetails
     && (
       <div>
+        <button type="button" className="start-recipe-btn" data-testid="start-recipe-btn">
+          Start Recipe
+        </button>
         <img data-testid="recipe-photo" src={ strDrinkThumb } alt={ strDrink } />
         <h1 data-testid="recipe-title">{ strDrink }</h1>
         <h3 data-testid="recipe-category">{ strAlcoholic }</h3>
--- src/components/MealsDetails.jsx
+++ src/components/MealsDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import RecipesAppContext from '../context/RecipesAppContext';
 
 function MealsDetails() {
@@ -11,17 +11,27 @@ function MealsDetails() {
     strYoutube,
   } = recipeDetails;
 
+  useEffect(() => {
+    const doneRecipesStorage = localStorage.getItem('doneRecipes');
+    console.log(doneRecipesStorage);
+    if (doneRecipesStorage) {
+      const resultStorage = JSON.parse(doneRecipesStorage);
+      console.log(resultStorage);
+    }
+  }, []);
+
   const entries = Object.entries(recipeDetails);
   const details = entries
     .filter((entrie) => entrie[1] !== null && entrie[1] !== '');
   const ingredients = details.filter((ing) => ing[0].includes('strIngredient'));
   const measures = details.filter((mea) => mea[0].includes('strMeasure'));
-  console.log(ingredients);
-  console.log(measures);
   return (
     recipeDetails
     && (
       <div>
+        <button type="button" className="start-recipe-btn" data-testid="start-recipe-btn">
+          Start Recipe
+        </button>
         <img data-testid="recipe-photo" src={ strMealThumb } alt={ strMeal } />
         <h1 data-testid="recipe-title">{ strMeal }</h1>
         <h3 data-testid="recipe-category">{ strCategory }</h3>
