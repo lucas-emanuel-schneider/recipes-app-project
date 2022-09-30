7bbdfed2c81d21e9aee5ae495b8c69fd4cb44796
tormelo <23294699+tormelo@users.noreply.github.com>
2022-09-30 10:20:21 -0300

    fix testes SearchBar

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -5,6 +5,7 @@ import App from '../App';
 import renderWithRouter from './helpers/renderWithRouter';
 import mealsMock from './helpers/mealsMock';
 import oneMeal from '../../cypress/mocks/oneMeal';
+import oneDrink from '../../cypress/mocks/oneDrink';
 
 const SEARCH_TOP_BTN_ID = 'search-top-btn';
 const SEARCH_INPUT_ID = 'search-input';
@@ -37,7 +38,7 @@ describe('Testa o componente SearchBar', () => {
       });
     });
   });
-  it('Deve redirecionar se só retornar um resultado apos filtro', async () => {
+  it('Deve redirecionar ao retornar só um resultado apos busca de meal', async () => {
     global.fetch = jest.fn().mockResolvedValue({
       json: jest.fn().mockResolvedValue(oneMeal),
     });
@@ -49,14 +50,36 @@ describe('Testa o componente SearchBar', () => {
 
     expect(screen.getByTestId(SEARCH_INPUT_ID)).toBeInTheDocument();
     expect(screen.getByTestId(EXEC_BTN_ID)).toBeInTheDocument();
-    userEvent.type(screen.getByTestId(SEARCH_INPUT_ID), 'orange');
+    userEvent.type(screen.getByTestId(SEARCH_INPUT_ID), 'meat');
     userEvent.click(screen.getByTestId(EXEC_BTN_ID));
 
     await waitFor(() => {
       expect(global.fetch).toHaveBeenCalled();
-      expect(global.fetch).toBeCalledWith('https://www.themealdb.com/api/json/v1/1/filter.php?i=orange');
+      expect(global.fetch).toBeCalledWith('https://www.themealdb.com/api/json/v1/1/filter.php?i=meat');
     });
 
     expect(history.location.pathname).toBe('/meals/52771');
+  });
+  it('Deve redirecionar ao retornar só um resultado apos busca de drink', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: jest.fn().mockResolvedValue(oneDrink),
+    });
+
+    const { history } = renderWithRouter(<App />, { initialEntries: ['/drinks'] });
+
+    expect(screen.getByTestId(SEARCH_TOP_BTN_ID)).toBeInTheDocument();
+    userEvent.click(screen.getByTestId(SEARCH_TOP_BTN_ID));
+
+    expect(screen.getByTestId(SEARCH_INPUT_ID)).toBeInTheDocument();
+    expect(screen.getByTestId(EXEC_BTN_ID)).toBeInTheDocument();
+    userEvent.type(screen.getByTestId(SEARCH_INPUT_ID), 'gin');
+    userEvent.click(screen.getByTestId(EXEC_BTN_ID));
+
+    await waitFor(() => {
+      expect(global.fetch).toHaveBeenCalled();
+      expect(global.fetch).toBeCalledWith('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=gin');
+    });
+
+    expect(history.location.pathname).toBe('/drinks/178319');
   });
 });
